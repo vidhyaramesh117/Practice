8d87311104af0fef590bd0e09f10cc0f582cafb2
vidhya <vidhyaramesh117@gmail.com>
2023-05-01 14:42:34 +0500

    google maps till marker doing

---
+++ GoogleMaps/app/src/main/java/com/example/googlemaps/MapsActivity.java
@@ -0,0 +1,42 @@
+package com.example.googlemaps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.googlemaps.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback
+//FragmentActivity
+ {
+
+    private GoogleMap map;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+    }
+
+
+     @Override
+     public void onMapReady(@NonNull GoogleMap googleMap)
+     {
+        map = googleMap;
+        LatLng latLng = new LatLng(10.1146, 76.4778)
+        MarkerOptions markerOptions = new MarkerOptions().position()
+        map.addMarker()
+     }
+ }
