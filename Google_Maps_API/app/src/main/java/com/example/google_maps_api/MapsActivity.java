b4593aea4e02983cdf4634d8fa881f84317e15c1
vidhya <vidhyaramesh117@gmail.com>
2023-05-08 14:37:28 +0500

    full 4 program with error in display

--- Google_Maps_API/app/src/main/java/com/example/google_maps_api/MapsActivity.java
+++ Google_Maps_API/app/src/main/java/com/example/google_maps_api/MapsActivity.java
@@ -25,15 +25,16 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.google_maps_api.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends AppCompatActivity
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback
 {
 
     private static final String TAG = "MapActivity";
-    private static final String FINE_LOCATION = com.example.google_maps_api.Manifest.permission.ACCESS_FINE_LOCATION;
-    private static final String COURSE_LOCATION= com.example.google_maps_api.Manifest.permission.ACCESS_COARSE_LOCATION;
-    private static final int LOCATION_PERMISSION_REQUEST_CODE  1234;
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
 
     private Boolean locationPermissionGranted = false;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -41,14 +42,22 @@ public class MapsActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        getLocationPermission();
 
+    }
 
+    private void initMap()
+    {
+        Log.d(TAG, "initMap: Intializing Map");
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        supportMapFragment.getMapAsync(MapsActivity.this);
     }
 
     private void getLocationPermission()
     {
+        Log.d(TAG, "getLocationPermission: Getting location");
         String[] permissions = { Manifest.permission.ACCESS_FINE_LOCATION,
-                com.example.google_maps_api.Manifest.permission.ACCESS_COARSE_LOCATION };
+                Manifest.permission.ACCESS_COARSE_LOCATION};
        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
        {
            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
@@ -61,25 +70,24 @@ public class MapsActivity extends AppCompatActivity
            }
        }
     }
-    @Override
-    public void onRequestPermissionResult(int request_code, @NonNull String[] permissions,@NonNull int[] grant_results)
-    {
-        locationPermissionGranted = false;
 
-        switch (request_code)
-        {
-            case LOCATION_PERMISSION_REQUEST_CODE:
-            {
-                if (grant_results.length >0)
-                {
-                    for (int i = 0; i < grant_results.length; i++)
-                    {
-                        if (grant_results[i] != PackageManager.PERMISSION_GRANTED)
-                        {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(TAG,"onRequestPermissionResult: Called");
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
                             locationPermissionGranted = false;
+                            Log.d(TAG, "onRequestPermissionsResult: Permission  failed");
                             return;
                         }
+                        Log.d(TAG, "onRequestPermissionsResult: Permission granted");
                         locationPermissionGranted = true;
+                        initMap();
                     }
                 }
             }
@@ -87,4 +95,11 @@ public class MapsActivity extends AppCompatActivity
     }
 
 
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap)
+    {
+        map = googleMap;
+        Log.d(TAG,"Map is ready");
+        Toast.makeText(this, "Map is ready", Toast.LENGTH_SHORT).show();
+    }
 }
